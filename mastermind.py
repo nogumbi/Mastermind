61cf0281aa1775b3eee5c74591e42d4000e3341b
Nobantu <67958932+nogumbi@users.noreply.github.com>
2020-11-09 08:30:12 +0200

    Mastermind 3

--- mastermind.py
+++ mastermind.py
@@ -1,63 +1,91 @@
 import random
 
-"""
-Generates the random code without duplictes
-"""
-def select_random_num():
-    code = [0,0,0,0]
+def create_code():
+    """Function that creates the 4 digit code, using random digits from 1 to 8"""
+
+    code = [0, 0, 0, 0]
+
     for i in range(4):
         value = random.randint(1, 8) # 8 possible digits
         while value in code:
             value = random.randint(1, 8)  # 8 possible digits
         code[i] = value
-    print('4-digit Code has been set. Digits in range 1 to 8. You have 12 turns to break it.')
     return code
 
-"""
-Gets the user input
-"""
-def get_user_input():
+def get_answer_input():
     answer = input("Input 4 digit code: ")
     if len(answer) < 4 or len(answer) > 4:
         print("Please enter exactly 4 digits.")
-        return get_user_input()
+        return get_answer_input()
     else:
         return answer
 
-"""
-Checks if the code is correct or not an gives feedback
-"""
-def give_feedback(answer, code):
-    correct = False
-    turns = 0
-    while not correct and turns < 12:
-        answer = get_user_input() 
 
-        correct_digits_and_position = 0
-        correct_digits_only = 0
-        for i in range(len(answer)):
-            if code[i] == int(answer[i]):
-                correct_digits_and_position += 1
-            elif int(answer[i]) in code:
-                correct_digits_only += 1
-        print('Number of correct digits in correct place:     '+str(correct_digits_and_position))
-        print('Number of correct digits not in correct place: '+str(correct_digits_only))
-        turns += 1
-        if correct_digits_and_position == 4:
-            correct = True
-            print('Congratulations! You are a codebreaker!')
-        else:
-            print('Turns left: '+str(12 - turns))
+def show_instructions():
+    """Shows instructions to the user"""
+
+    print('4-digit Code has been set. Digits in range 1 to 8. You have 12 turns to break it.')
+
+
+def show_results(correct_digits_and_position, correct_digits_only):
+    """Show the results from one turn"""
+
+    print('Number of correct digits in correct place:     ' + str(correct_digits_and_position))
+    print('Number of correct digits not in correct place: ' + str(correct_digits_only))
+
+
+def take_turn(code):
+    """Handle the logic of taking a turn, which includes:
+       * get answer from user
+       * check if answer is valid
+       * check correctness of answer
+    """
+    answer = get_answer_input()
+
+    correct_digits_and_position = 0
+    correct_digits_only = 0
+    for i in range(len(answer)):
+        if code[i] == int(answer[i]):
+            correct_digits_and_position += 1
+        elif int(answer[i]) in code:
+            correct_digits_only += 1
+
+    show_results(correct_digits_and_position, correct_digits_only)
+    return (correct_digits_and_position, correct_digits_only)
+
+def show_code(code):
+    """Show Code that was created to user"""
 
     print('The code was: '+str(code))
 
-"""
-Calls all the other functions
-"""
+
+def check_correctness(turns, correct_digits_and_position):
+    """Checks correctness of answer and show output to user"""
+
+    if correct_digits_and_position == 4:
+        print('Congratulations! You are a codebreaker!')
+        return True
+    else:
+        print('Turns left: ' + str(12 - turns))
+        return False
+
 def run_game():
-    code = select_random_num()
-    answer = []
-    give_feedback(answer, code)
+    """Main function for running the game"""
+
+    #global correct
+    correct = False
+
+    code = create_code()
+    show_instructions()
+
+    turns = 0
+    while not correct and turns < 12:
+        Tuple = take_turn(code)
+        turns += 1
+        correct = check_correctness(turns, Tuple[0])
+
+    show_code(code)
+
 
 if __name__ == "__main__":
     run_game()
