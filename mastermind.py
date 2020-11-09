bc4b17f9730f6ba1fa089d53a4b439d1b50959aa
Nobantu <67958932+nogumbi@users.noreply.github.com>
2020-11-09 08:28:56 +0200

    Mastermind 2

--- mastermind.py
+++ mastermind.py
@@ -1,50 +1,63 @@
 import random
 
-def run_game():
+"""
+Generates the random code without duplictes
+"""
+def select_random_num():
+    code = [0,0,0,0]
+    for i in range(4):
+        value = random.randint(1, 8) # 8 possible digits
+        while value in code:
+            value = random.randint(1, 8)  # 8 possible digits
+        code[i] = value
+    print('4-digit Code has been set. Digits in range 1 to 8. You have 12 turns to break it.')
+    return code
 
-    num = []    
+"""
+Gets the user input
+"""
+def get_user_input():
+    answer = input("Input 4 digit code: ")
+    if len(answer) < 4 or len(answer) > 4:
+        print("Please enter exactly 4 digits.")
+        return get_user_input()
+    else:
+        return answer
 
-    #STEP 1: Guess a code
-    while len(num) < 4:
-        rand_num = random.randint(1, 8)
-        if rand_num not in num:
-            num.append(rand_num)
-    print('4-digit Code has been set. Digits in range 1 to 8. You have 12 turns to break it.')    
-    
-    #STEP 2: Get a guess
-    turns = 12
-    user_num = ''
-    while turns > 0:
-        user_num = input('Input 4 digit code: ')
-        if not user_num.isdigit() or ('0' in user_num or '9' in user_num ) or len(user_num) != 4: #combined all the conditions into a single if statement 
-            print('Please enter exactly 4 digits.')
-            continue #continue added for the loop to return to the beginning if the "if" statement returns true
-        # except EOFError:
-        #     print('') 
+"""
+Checks if the code is correct or not an gives feedback
+"""
+def give_feedback(answer, code):
+    correct = False
+    turns = 0
+    while not correct and turns < 12:
+        answer = get_user_input() 
 
-        #STEP 3: Evaluate input
-        correct_digits_in_correct_place = 0
-        correct_digits_not_in_correct_place = 0
-        for i in range(len(user_num)):
-            if int(user_num[i]) == num[i]:
-                correct_digits_in_correct_place += 1
-            elif int(user_num[i]) in num:
-                correct_digits_not_in_correct_place += 1
-        print(f'Number of correct digits in correct place:     {correct_digits_in_correct_place}')
-        print(f'Number of correct digits not in correct place: {correct_digits_not_in_correct_place}')  
-        
-        if correct_digits_in_correct_place == 4:
-            empty_word = ''
-            for i in range(4):
-                empty_word += str(num[i])
+        correct_digits_and_position = 0
+        correct_digits_only = 0
+        for i in range(len(answer)):
+            if code[i] == int(answer[i]):
+                correct_digits_and_position += 1
+            elif int(answer[i]) in code:
+                correct_digits_only += 1
+        print('Number of correct digits in correct place:     '+str(correct_digits_and_position))
+        print('Number of correct digits not in correct place: '+str(correct_digits_only))
+        turns += 1
+        if correct_digits_and_position == 4:
+            correct = True
             print('Congratulations! You are a codebreaker!')
-            print(f'The code was: {empty_word}')
-            break
         else:
-            #STEP 4: Count guesses
-            turns -= 1
-            print(f'Turns left: {turns}')
+            print('Turns left: '+str(12 - turns))
+
+    print('The code was: '+str(code))
+
+"""
+Calls all the other functions
+"""
+def run_game():
+    code = select_random_num()
+    answer = []
+    give_feedback(answer, code)
 
 if __name__ == "__main__":
     run_game()
-
